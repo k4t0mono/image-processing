38b88fc244081f6e8514862ef65b9eca16e3cdfe
KatoMono Enkeli <k4t0mono@gmail.com>
2018-09-06 23:54:23 -0300

    Bluring image, except in the edges

--- src/image.cpp
+++ src/image.cpp
@@ -74,8 +74,10 @@ Image::get_at(unsigned int x, unsigned int y)
 Pixel
 Image::set_at(const Pixel& px, unsigned int x, unsigned int y)
 {
-	if(x >= this->width || y >= this->height)
+	if(x >= this->width || y >= this->height) {
+		std::cerr << "(" << x << ", " << y << ")" << std::endl;
 		throw std::out_of_range("Invalid position for this image");
+	}
 
 	unsigned int i = x * this->width + y;
 	Pixel p = this->pixels[i];
--- src/mean_filter.cpp
+++ src/mean_filter.cpp
@@ -46,34 +46,35 @@ mean_filter(const Image& src, int mx, int my)
 
 	Image mod = src;
 	Kernel k = gen_kernel(mx, my);
-	std::cout << k.data[0][0].x << std::endl;
-	printf("%dx%d\n", mod.width, mod.height);
 
-	int x = 1;
-	int y = 8;
-	std::cout << mod.get_at(x, y) << std::endl;
+	for(unsigned int x = 1; x < src.height-1; ++x) {
+		for(unsigned int y = 1; y < src.width-1; ++y) {
+			printf("(%d, %d): ", x, y);
+			std::cout << "op: " << mod.get_at(x, y) << "\t";
 
-	Pixel np;
-	for(int i = 0; i < mx; i++) {
-		for(int j = 0; j < my; j++) {
-			printf("k: (% 2d, % 2d, %.3f)\t", k.data[i][j].x,k.data[i][j].y,k.data[i][j].factor);
-			int kx = k.data[i][j].x + x;
-			int ky = k.data[i][j].y + y;
-			float f = k.data[i][j].factor;
+			Pixel np;
+			for(int i = 0; i < mx; i++) {
+				for(int j = 0; j < my; j++) {
+					//printf("k: (% 2d, % 2d, %.3f)\t", k.data[i][j].x,k.data[i][j].y,k.data[i][j].factor);
+					int kx = k.data[i][j].x + x;
+					int ky = k.data[i][j].y + y;
+					float f = k.data[i][j].factor;
 
-			printf("px(%d, %d) = ", kx, ky);
-			std::cout << mod.get_at(kx, ky) << std::endl;
+					//printf("px(%d, %d) = ", kx, ky);
+					//std::cout << mod.get_at(kx, ky) << std::endl;
 
-			Pixel op = mod.get_at(kx, ky);
-			np.red += op.red * f;
-			np.green += op.green * f;
-			np.blue += op.blue * f;
-			np.alpha = 0xff;
+					Pixel op = mod.get_at(kx, ky);
+					np.red += op.red * f;
+					np.green += op.green * f;
+					np.blue += op.blue * f;
+					np.alpha = 0xff;
+				}
+			}
+
+			std::cout << "np: " << np << std::endl;
+			mod.set_at(np, x, y);
 		}
 	}
-
-	std::cout << "np: " << np << std::endl;
-	mod.set_at(np, x, y);
 
 	return mod;
 }
